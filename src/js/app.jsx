7ee2709bb8c0c33a655a6d69a819004d615fafdd
Lypps <r_lypps@yahoo.com>
2020-01-14 13:42:24 -0800

    Done

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import CurrentCity from "./components/CurrentCity"
+import SearchCity from './components/SearchCity'
+import SearchHistory from "./components/SearchHistory"
+import TitleHeader from "./components/TitleHeader"
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
+          <TitleHeader />
+        <div className="row">
+          <SearchCity />
+        </div>
+        <div className="row">
+          <div className="col-md-7">
+            <p></p>
+            <CurrentCity />
+          </div>
+          <div className="col-md-5">
+            <br></br>
+            <br></br>
+            <SearchHistory />
+          </div>
+        </div>
       </div>
     );
   }
--- src/js/rootReducer.js
+++ src/js/rootReducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import SearchCityReducer from './components/SearchCity/SearchCityReducer';
 
 const rootReducer = combineReducers({
 // add reducers
+    search: SearchCityReducer
+
 });
 
 export default rootReducer;
